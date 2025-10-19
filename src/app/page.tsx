665a91abdd202811f459c14bfb9e4bbae17b3ecf
Hazique <khanhazique04@gmail.com>
2025-10-20 03:51:24 +0530

    02:Prisma setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
-const Page =()=>{
+import prisma from "@/lib/db"
+
+const Page =async ()=>{
+
+  const users = await prisma.user.findMany();
+
 
   return (
-    <div>
-      Hello 
+    <div className="min-h-screen min-w-screen flex items-center justify-center">
+     {JSON.stringify(users)}
     </div>
   )
 
